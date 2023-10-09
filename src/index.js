22465b2d277009960a2d22515ab4b7d52415c778
Vladimir <scofari99@gmail.com>
2023-10-09 21:08:40 +0300

    add ScrollToTop component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 import "./index.css";
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
